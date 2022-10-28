b0cccea89a3edb56fc0f1a33f12ae01477728126
TomasHaro <86988905+TomasHaro@users.noreply.github.com>
2022-10-28 13:45:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# App-Angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-koahb3)
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from "@angular/forms";
+import {NgbPaginationModule} from "@ng-bootstrap/ng-bootstrap";
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
+  
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, NgbPaginationModule, 
+    ReactiveFormsModule, 
+    RouterModule.forRoot([
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'inicio', component: InicioComponent },
+    { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
+    { path: 'articulos', component: ArticulosComponent }
+  ]) ],
+  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent, MenuComponent, ArticulosComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' }]
+})
+export class AppModule { }
+
